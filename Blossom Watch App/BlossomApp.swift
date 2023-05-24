145f22a697c8d7b71c5856a9db84e61dbce301e2
Achmad Syarief Thalib <achmad_st@apps.ipb.ac.id>
2023-05-24 12:32:15 +0700

    Initial Commit

---
+++ Blossom Watch App/BlossomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomApp.swift
+//  Blossom Watch App
+//
+//  Created by measthmatic on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Blossom_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
